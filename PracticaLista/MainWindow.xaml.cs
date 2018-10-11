168f1df5321bb4534da172a610c34b76c7e16154
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-11 08:37:13 -0700

    Agregar archivos de proyecto.

---
+++ PracticaLista/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaLista
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Alumno> alumnos = new List<Alumno>(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            alumnos.Add(new Alumno("Juan Pérez", "153697", "Lic. en Derecho"));
+            alumnos.Add(new Alumno("Pedro Lopez", "786132", "Lic. en Comunicación"));
+            alumnos.Add(new Alumno("María García", "325617", "Lic. en Psicología"));
+            alumnos.Add(new Alumno("Ana Valenzuela", "238546", "Lic. en Finanzas"));
+
+            foreach (Alumno alumno in alumnos)
+            {
+                lstAlumnos.Items.Add(new ListBoxItem()
+                {
+                    Content = alumno.Nombre
+                }
+                );
+            }
+        }
+
+        private void lstAlumnos_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblNombre.Text = alumnos[lstAlumnos.SelectedIndex].Nombre;
+            lblMatricula.Text = alumnos[lstAlumnos.SelectedIndex].Matricula;
+            lblCarrera.Text = alumnos[lstAlumnos.SelectedIndex].Carrera;
+        }
+    }
+}
